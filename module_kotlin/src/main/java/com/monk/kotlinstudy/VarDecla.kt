59e62b7dace0b8d16e21a1fd10d75932aa9b02bf
hyh <1109466796@qq.com>
2021-01-29 18:14:46 +0800

    vc=6，写个kotlin文件

--- module_kotlin/src/main/java/com/monk/kotlinstudy/VarDecla.kt
+++ module_kotlin/src/main/java/com/monk/kotlinstudy/VarDecla.kt
@@ -1,5 +1,7 @@
 package com.monk.kotlinstudy
 
+import kotlin.concurrent.thread
+
 /***
  * - var 声明变量
  * - val 声明常量
@@ -17,4 +19,55 @@ fun main() {
 
     val b: Int = 0b0011 // 用0b来表示二进制
     println("b的十进制：$b")
+
+    val s = "abc"
+    println("$s.length is ${s.length}")
+
+    val range = 0..10 // 数学中的[0, 10]
+    val range1 = 0 until 10 // 数学中的[0, 10)
+    println(range)
+    println(range1)
+
+    forStatement()
+
+    val list = listOf("a","b")
+    val list2 = mutableListOf("a","b")
+
+    Thread { TODO("Not yet implemented") }
+    Thread(object :Runnable{
+        override fun run() {
+            TODO("Not yet implemented")
+        }
+    })
+
+}
+
+fun forStatement() {
+    // 遍历[0, 10]中的每一个元素
+    for (i in 0..10) print(i)
+    println()
+
+    // 遍历[0, 10)的时候，每次循环会在区间范围内递增2，相当于 for-i 中的 i = i + 2 效果
+    // step 关键字可以跳过其中一些元素
+    for (i in 0 until 10 step 2)print(i)
+    println()
+
+    // 降序遍历[0, 10]中的每一个元素
+    // downTo 关键字用来创建降序的空间
+    for (i in 10 downTo 1)print(i)
+    println()
+}
+
+fun getScore(name: String) = when (name) {
+    "Wonderful" -> 100
+    "Tome" -> 86
+    "Jack" -> 60
+    else -> 0
+}
+
+fun doStudy(name:String?){
+    if (name!=null) println(name)
+}
+fun doStudy2(name:String){
+    println(name)
 }
---
+++ module_kotlin/src/main/java/com/monk/kotlinstudy/extends/Person.kt
@@ -0,0 +1,37 @@
+package com.monk.kotlinstudy.extends
+
+open class Person {
+
+    private var name: String? = null
+    private var age: Int?=0
+
+    fun eat() {
+        println("$name is eating. He is $age years old ")
+    }
+
+
+}
+
+/*** 主构造函数*/
+class Student(name: String, age: Int) : Person() {
+
+    init {
+        println("init")
+    }
+
+    /*** 次构造函数*/
+    constructor(name: String) : this("", 12) {}
+    constructor() : this("", 12) {}
+
+}
+
+class Student2 : Person{
+    constructor(name:String,age: Int) : super(){}
+}
+
+data class Bean(val name: String, val innerBean: InnerBean){
+    data class InnerBean(val name: String)
+}
+
+object SingleTon{
+}
