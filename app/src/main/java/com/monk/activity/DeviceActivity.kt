d398f37661d1af3c5ab042c33932d5956b26fc8c
hyh <1109466796@qq.com>
2021-05-12 09:40:36 +0800

    vc=12,vs=2.0,拆分

--- app/src/main/java/com/monk/activity/DeviceActivity.kt
+++ app/src/main/java/com/monk/activity/DeviceActivity.kt
@@ -5,16 +5,15 @@ import androidx.appcompat.widget.AppCompatCheckedTextView
 import butterknife.BindView
 import com.monk.activity.base.BaseCompatActivity
 import com.monk.aidldemo.R
-import com.monk.commonutils.DeviceHelper.Companion.instance
+import com.monk.commonutils.DeviceHelper
 
 class DeviceActivity : BaseCompatActivity<DeviceActivity?>() {
     @BindView(R.id.textView)
     var textView: AppCompatCheckedTextView? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initToolbar(R.layout.activity_device)
-        val deviceMsg = instance.getDeviceMsg(this)
+        val deviceMsg = DeviceHelper.instance.getDeviceMsg(this)
         textView!!.text = deviceMsg
     }
 }
--- commonutils/src/main/java/com/monk/commonutils/DeviceHelper.kt
+++ commonutils/src/main/java/com/monk/commonutils/DeviceHelper.kt
@@ -15,12 +15,12 @@ class DeviceHelper private constructor(){
         val holder=DeviceHelper()
     }
 
-    fun getDeviceMsg(context: Context?): String? {
+    fun getDeviceMsg(context: Context): String? {
         val sb = StringBuilder()
         sb.append("设备信息：" + DeviceUtils.decompile()).append("\n")
-                .append("设备信息：" + DeviceUtils.getModel()).append("\n")
+                .append("设备信息：" + DeviceUtils.model).append("\n")
                 .append("内存占用信息：" + DeviceUtils.getMaxMemoryInfo(context)).append("\n")
-                .append("设备屏幕信息：" + DeviceUtils.getScreenHeightAndWidth(context as Activity?)).append("\n")
+                .append("设备屏幕信息：" + DeviceUtils.getScreenHeightAndWidth(context as Activity)).append("\n")
                 .append("设备屏幕密度：" + DeviceUtils.getDensityAndDensityDpi(context)).append("\n")
                 .append("")
         return sb.toString()
