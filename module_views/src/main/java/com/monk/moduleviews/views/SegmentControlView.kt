fe626ac7fa829f13d0e88e37071b38511e67c729
m <1109466796@qq.com>
2021-05-13 21:01:48 +0800

    vc=12,vs=2.0 continue k

--- module_views/src/main/java/com/monk/moduleviews/views/SegmentControlView.kt
+++ module_views/src/main/java/com/monk/moduleviews/views/SegmentControlView.kt
@@ -1,138 +1,137 @@
-package com.monk.moduleviews.views;
+package com.monk.moduleviews.views
 
-import android.content.Context;
-import android.content.res.ColorStateList;
-import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.monk.moduleviews.R;
-
-import org.xmlpull.v1.XmlPullParser;
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.monk.moduleviews.R
+import org.xmlpull.v1.XmlPullParser
 
 /**
  * @author JackieHank
  * @date 2017-09-08 16:09.
  */
-public class SegmentControlView extends LinearLayout {
+class SegmentControlView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int)
+    : LinearLayout(context, attrs, defStyleAttr) {
 
-    private TextView textView1;
-    private TextView textView2;
+    private var textView1: TextView? = null
+    private var textView2: TextView? = null
 
-    public SegmentControlView(Context context) {
-        this(context,null);
-        initView();
-    }
-    public SegmentControlView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
-        initView();
-    }
-    public SegmentControlView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initView();
+    constructor(context: Context?) : this(context, null) {
+        initView()
     }
 
-    private void initView() {
-        textView1 = new TextView(getContext());
-        textView2 = new TextView(getContext());
-        textView1.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-        textView2.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-        textView1.setText("错题");
-        textView2.setText("收藏");
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0) {
+        initView()
+    }
+
+    private fun initView() {
+        textView1 = TextView(context)
+        textView2 = TextView(context)
+        textView1!!.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        textView2!!.layoutParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        textView1!!.text = "错题"
+        textView2!!.text = "收藏"
         try {
-            XmlPullParser xrp = getResources().getXml(R.xml.selector_segmentcontrol);
-            ColorStateList csl = ColorStateList.createFromXml(getResources(), xrp);
-            textView1.setTextColor(csl);
-            textView2.setTextColor(csl);
-        } catch (Exception e) {
+            val xrp: XmlPullParser = resources.getXml(R.xml.selector_segmentcontrol)
+            val csl = ColorStateList.createFromXml(resources, xrp)
+            textView1!!.setTextColor(csl)
+            textView2!!.setTextColor(csl)
+        } catch (e: Exception) {
         }
-        textView1.setGravity(Gravity.CENTER);
-        textView2.setGravity(Gravity.CENTER);
-        textView1.setPadding(dp2Px(getContext(),10), dp2Px(getContext(),5), dp2Px(getContext(),10), dp2Px(getContext(),5));
-        textView2.setPadding(dp2Px(getContext(),10), dp2Px(getContext(),5), dp2Px(getContext(),10), dp2Px(getContext(),5));
-        setSegmentTextSize(18);
-        textView1.setBackgroundResource(R.drawable.segment_left);
-        textView2.setBackgroundResource(R.drawable.segment_right);
-        textView1.setSelected(true);
-        this.removeAllViews();
-        this.addView(textView1);
-        this.addView(textView2);
-        this.invalidate();
-
-        textView1.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (textView1.isSelected()) {
-                    return;
-                }
-                textView1.setSelected(true);
-                textView2.setSelected(false);
-                if (listener != null) {
-                    listener.onSegmentViewClick(textView1, 0);
-                }
+        textView1!!.gravity = Gravity.CENTER
+        textView2!!.gravity = Gravity.CENTER
+        textView1!!.setPadding(dp2Px(context, 10f), dp2Px(context, 5f), dp2Px(context, 10f), dp2Px(context, 5f))
+        textView2!!.setPadding(dp2Px(context, 10f), dp2Px(context, 5f), dp2Px(context, 10f), dp2Px(context, 5f))
+        setSegmentTextSize(18)
+        textView1!!.setBackgroundResource(R.drawable.segment_left)
+        textView2!!.setBackgroundResource(R.drawable.segment_right)
+        textView1!!.isSelected = true
+        removeAllViews()
+        this.addView(textView1)
+        this.addView(textView2)
+        this.invalidate()
+        textView1!!.setOnClickListener(OnClickListener {
+            if (textView1!!.isSelected) {
+                return@OnClickListener
             }
-        });
-        textView2.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (textView2.isSelected()) {
-                    return;
-                }
-                textView2.setSelected(true);
-                textView1.setSelected(false);
-                if (listener != null) {
-                    listener.onSegmentViewClick(textView2, 1);
-                }
+            textView1!!.isSelected = true
+            textView2!!.isSelected = false
+            if (listener != null) {
+                listener!!.onSegmentViewClick(textView1, 0)
             }
-        });
+        })
+        textView2!!.setOnClickListener(OnClickListener {
+            if (textView2!!.isSelected) {
+                return@OnClickListener
+            }
+            textView2!!.isSelected = true
+            textView1!!.isSelected = false
+            if (listener != null) {
+                listener!!.onSegmentViewClick(textView2, 1)
+            }
+        })
     }
 
     /**
      * 设置字体大小 单位dip
-     * <p>2014年7月18日</p>
+     *
+     * 2014年7月18日
      * @param dp
      * @author RANDY.ZHANG
      */
-    public void setSegmentTextSize(int dp) {
-        textView1.setTextSize(TypedValue.COMPLEX_UNIT_DIP, dp);
-        textView2.setTextSize(TypedValue.COMPLEX_UNIT_DIP, dp);
+    fun setSegmentTextSize(dp: Int) {
+        textView1!!.setTextSize(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat())
+        textView2!!.setTextSize(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat())
     }
 
-    private static int dp2Px(Context context, float dp) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
+    private var listener: onSegmentViewClickListener? = null
+    fun setOnSegmentViewClickListener(listener: onSegmentViewClickListener?) {
+        this.listener = listener
     }
-    private onSegmentViewClickListener listener;
-    public void setOnSegmentViewClickListener(onSegmentViewClickListener listener) {
-        this.listener = listener;
-    }
+
     /**
      * 设置文字
-     * <p>2014年7月18日</p>
+     *
+     * 2014年7月18日
      * @param text
      * @param position
      * @author RANDY.ZHANG
      */
-    public void setSegmentText(CharSequence text, int position) {
+    fun setSegmentText(text: CharSequence?, position: Int) {
         if (position == 0) {
-            textView1.setText(text);
+            textView1!!.text = text
         }
         if (position == 1) {
-            textView2.setText(text);
+            textView2!!.text = text
         }
     }
 
-    public interface onSegmentViewClickListener{
+    interface onSegmentViewClickListener {
         /**
          *
-         * <p>2014年7月18日</p>
+         *
+         * 2014年7月18日
          * @param v
          * @param position 0-左边 1-右边
          * @author RANDY.ZHANG
          */
-        void onSegmentViewClick(View v, int position);
+        fun onSegmentViewClick(v: View?, position: Int)
+    }
+
+    companion object {
+        private fun dp2Px(context: Context, dp: Float): Int {
+            val scale = context.resources.displayMetrics.density
+            return (dp * scale + 0.5f).toInt()
+        }
+    }
+
+    init {
+        initView()
     }
 }
--- module_views/src/main/java/com/monk/moduleviews/views/ZoomImageView.kt
+++ module_views/src/main/java/com/monk/moduleviews/views/ZoomImageView.kt
@@ -1,157 +1,132 @@
-package com.monk.moduleviews.views;
+package com.monk.moduleviews.views
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Matrix;
-import android.graphics.PointF;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.MotionEvent;
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatImageView
+import kotlin.math.sqrt
 
-import androidx.appcompat.widget.AppCompatImageView;
+class ZoomImageView(context: Context?, attrs: AttributeSet?) : AppCompatImageView(context!!, attrs) {
+    private val matrixs = Matrix()
+    private val savedMatrix = Matrix()
+    private var mode = NONE
 
-
-public class ZoomImageView extends AppCompatImageView {
-    private final Matrix matrix = new Matrix();
-    private final Matrix savedMatrix = new Matrix();
-
-    private static final int NONE = 0;
-    private static final int DRAG = 1;
-    private static final int ZOOM = 2;
-    private int mode = NONE;
-    private static final float MAX_SCALE = 3;
-    private static final float mCurScale = 1.0f;
-    private static float mStartScale;
     // 第一个按下的手指的点
-    private final PointF startPoint = new PointF();
+    private val startPoint = PointF()
+
     // 两个按下的手指的触摸点的中点
-    private PointF midPoint = new PointF();
+    private var midPoint = PointF()
+
     // 初始的两个手指按下的触摸点的距离
-    private float oriDis = 1f;
+    private var oriDis = 1f
+    private var count = 0
 
-    public ZoomImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        // mGestureDetector=new GestureDetector(context, new GestureListener());
-
+    companion object {
+        private const val NONE = 0
+        private const val DRAG = 1
+        private const val ZOOM = 2
+        private const val MAX_SCALE = 3f
+        private const val mCurScale = 1.0f
+        private var mStartScale = 0f
     }
 
-    private int count = 0;
-
-    @Override
-    protected void onDraw(Canvas canvas) {
+    override fun onDraw(canvas: Canvas) {
         // TODO Auto-generated method stub
-        super.onDraw(canvas);
+        super.onDraw(canvas)
         if (count == 0) {
-            float[] values = new float[9];
-            getImageMatrix().getValues(values);
+            val values = FloatArray(9)
+            imageMatrix.getValues(values)
+            mStartScale = values[Matrix.MSCALE_X]
+            Log.e("", "s=$mStartScale")
+            count++
+        }
+    }
 
-            mStartScale = values[Matrix.MSCALE_X];
-            Log.e("", "s=" + mStartScale);
-            count++;
+    // return scale!=1.0f;
+    private val isZoomChanged: Float
+        get() {
+            val values = FloatArray(9)
+            imageMatrix.getValues(values)
+            val scale = values[Matrix.MSCALE_X]
+            return if (scale != mStartScale) mStartScale / scale else 2.0f
+            // return scale!=1.0f;
         }
 
-    }
-
-    private float isZoomChanged() {
-        float[] values = new float[9];
-        getImageMatrix().getValues(values);
-
-        float scale = values[Matrix.MSCALE_X];
-        if (scale != mStartScale)
-            return mStartScale / scale;
-        else
-            return 2.0f;
-        // return scale!=1.0f;
-    }
-
-    @Override
-    public void setImageBitmap(Bitmap bm) {
+    override fun setImageBitmap(bm: Bitmap) {
         // TODO Auto-generated method stub
-        super.setImageBitmap(bm);
-
+        super.setImageBitmap(bm)
     }
 
-    private long startTime = 0;
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
+    private var startTime: Long = 0
+    override fun onTouchEvent(event: MotionEvent): Boolean {
         // 进行与操作是为了判断多点触摸
-        switch (event.getAction() & MotionEvent.ACTION_MASK) {
-            case MotionEvent.ACTION_DOWN:
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
                 // 第一个手指按下事件
-                setScaleType(ScaleType.MATRIX);
-                long interval = event.getEventTime() - startTime;
-                Log.e("", "now-st=" + interval + "st=" + startTime);
-
+                scaleType = ScaleType.MATRIX
+                val interval = event.eventTime - startTime
+                Log.e("", "now-st=" + interval + "st=" + startTime)
                 if (interval < 300) {
-                    Log.e("", "double click");
-                    matrix.set(getImageMatrix());
-
-                    float scale = isZoomChanged();
-                    Log.e("", "scale=" + scale);
-                    matrix.postScale(scale, scale, getWidth() / 2, getHeight() / 2);
-                    setImageMatrix(matrix);
-                    break;
+                    Log.e("", "double click")
+                    matrixs.set(imageMatrix)
+                    val scale = isZoomChanged
+                    Log.e("", "scale=$scale")
+                    matrixs.postScale(scale, scale, (width / 2).toFloat(), (height / 2).toFloat())
+                    imageMatrix = matrixs
+                }else{
+                    startTime = event.eventTime
+                    Log.e("", "down")
+                    Log.e("", "width=" + drawable.bounds.width())
+                    matrixs.set(imageMatrix)
+                    savedMatrix.set(matrixs)
+                    startPoint[event.x] = event.y
+                    mode = DRAG
                 }
-
-                startTime = event.getEventTime();
-                Log.e("", "down");
-                Log.e("", "width=" + getDrawable().getBounds().width());
-                matrix.set(getImageMatrix());
-                savedMatrix.set(matrix);
-                startPoint.set(event.getX(), event.getY());
-                mode = DRAG;
-                break;
-            case MotionEvent.ACTION_POINTER_DOWN:
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {
                 // 第二个手指按下事件
-
-                Log.e("", "double down");
-                oriDis = distance(event);
+                Log.e("", "double down")
+                oriDis = distance(event)
                 if (oriDis > 10f) {
-                    savedMatrix.set(matrix);
-                    midPoint = middle(event);
-                    mode = ZOOM;
+                    savedMatrix.set(matrixs)
+                    midPoint = middle(event)
+                    mode = ZOOM
                 }
-                break;
-            case MotionEvent.ACTION_UP:
-                // 手指放开事件
-                mode = NONE;
-                break;
-            case MotionEvent.ACTION_POINTER_UP:
-                // 手指放开事件
-                mode = NONE;
-                break;
-            case MotionEvent.ACTION_MOVE:
-
+            }
+            MotionEvent.ACTION_UP ->                 // 手指放开事件
+                mode = NONE
+            MotionEvent.ACTION_POINTER_UP ->                 // 手指放开事件
+                mode = NONE
+            MotionEvent.ACTION_MOVE ->
                 // 手指滑动事件
                 if (mode == DRAG) {
                     // 是一个手指拖动
-
-                    matrix.set(savedMatrix);
-                    Log.e("mmm", "event.getX() - startPoint.x＝" + event.getX()
-                            + "-" + startPoint.x);
-                    Log.e("mmm", matrix.toString());
-                    matrix.postTranslate(event.getX() - startPoint.x, event.getY()
-                            - startPoint.y);
-                    Log.e("mmm", "x=" + getX() + "y=" + getY());
+                    matrixs.set(savedMatrix)
+                    Log.e("mmm", "event.getX() - startPoint.x＝" + event.x + "-" + startPoint.x)
+                    Log.e("mmm", matrixs.toString())
+                    matrixs.postTranslate(event.x - startPoint.x, event.y - startPoint.y)
+                    Log.e("mmm", "x=" + x + "y=" + y)
                 } else if (mode == ZOOM) {
                     // 两个手指滑动
-                    float newDist = distance(event);
+                    val newDist = distance(event)
                     if (newDist > 10f) {
-                        matrix.set(savedMatrix);
-                        float scale = newDist / oriDis;
-                        float[] values = new float[9];
-                        matrix.getValues(values);
-                        scale = checkMaxScale(scale, values);
+                        matrixs.set(savedMatrix)
+                        var scale = newDist / oriDis
+                        val values = FloatArray(9)
+                        matrixs.getValues(values)
+                        scale = checkMaxScale(scale, values)
                     }
                 }
-                break;
         }
 
         // 设置ImageView的Matrix
-        this.setImageMatrix(matrix);
-        return true;
+        this.imageMatrix = matrixs
+        return true
     }
 
     /**
@@ -161,27 +136,25 @@ public class ZoomImageView extends AppCompatImageView {
      * @param values
      * @return
      */
-    private float checkMaxScale(float scale, float[] values) {
-        if (scale * values[Matrix.MSCALE_X] > MAX_SCALE)
-            scale = MAX_SCALE / values[Matrix.MSCALE_X];
-        else if (scale * values[Matrix.MSCALE_X] < mStartScale)
-            scale = mStartScale / values[Matrix.MSCALE_X];
-        matrix.postScale(scale, scale, getWidth() / 2, getHeight() / 2);
-        return scale;
+    private fun checkMaxScale(scale: Float, values: FloatArray): Float {
+        var scale = scale
+        if (scale * values[Matrix.MSCALE_X] > MAX_SCALE) scale = MAX_SCALE / values[Matrix.MSCALE_X] else if (scale * values[Matrix.MSCALE_X] < mStartScale) scale = mStartScale / values[Matrix.MSCALE_X]
+        matrixs.postScale(scale, scale, (width / 2).toFloat(), (height / 2).toFloat())
+        return scale
     }
 
     // 计算两个触摸点之间的距离
-    private float distance(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (float) Math.sqrt(x * x + y * y);
+    private fun distance(event: MotionEvent): Float {
+        val x = event.getX(0) - event.getX(1)
+        val y = event.getY(0) - event.getY(1)
+        return sqrt((x * x + y * y).toDouble()).toFloat()
     }
 
     // 计算两个触摸点的中点
-    private PointF middle(MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        return new PointF(x / 2, y / 2);
+    private fun middle(event: MotionEvent): PointF {
+        val x = event.getX(0) + event.getX(1)
+        val y = event.getY(0) + event.getY(1)
+        return PointF(x / 2, y / 2)
     }
 
 }
